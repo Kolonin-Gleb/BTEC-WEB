688d5bb61d6b02830b86c0adbd4f680a82eced03
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-19 19:32:02 +0300

    Процесс разработки сайта

---
+++ website/static/js/bouquet.js
@@ -0,0 +1,51 @@
+// Добавить img и путь к файлу
+// Это позволит Использовать мои картинки, а не импортированные иконки
+
+// Цветы, что есть в наличии
+// Все данные о цветах, что есть в наличии должны будут браться из БД и переноситься в этот массив объектов.
+
+let flowers = [
+    {name: "Gerbera", color: "white", count: 10, price: 200, img: "./Images/flowers/Gerberas.png"},
+    {name: "Gerbera", color: "yellow", count: 10, price: 200, img: "./Images/flowers/Gerberas.png"},
+    {name: "Gerbera", color: "pink", count: 10, price: 200, img: "./Images/flowers/Gerberas.png"},
+    {name: "Gerbera", color: "red", count: 10, price: 200, img: "./Images/flowers/Gerberas.png"},
+    {name: "Gerbera", color: "blue", count: 10, price: 200, img: "./Images/flowers/Gerberas.png"},
+
+    {name: "Lilies", color: "white", count: 10, price: 150, img: "./Images/flowers/Lilies.png"},
+    {name: "Lilies", color: "yellow", count: 10, price: 150, img: "./Images/flowers/Lilies.png"},
+    {name: "Lilies", color: "red", count: 10, price: 150, img: "./Images/flowers/Lilies.png"},
+
+    {name: "Piones", color: "white", count: 10, price: 150, img: "./Images/flowers/Piones.png"},
+    {name: "Piones", color: "yellow", count: 10, price: 150, img: "./Images/flowers/Piones.png"},
+    {name: "Piones", color: "red", count: 10, price: 150, img: "./Images/flowers/Piones.png"},
+
+    {name: "Rose", color: "white", count: 10, price: 300, img: "./Images/flowers/Roses.png"},
+    {name: "Rose", color: "yellow", count: 10, price: 300, img: "./Images/flowers/Roses.png"},
+    {name: "Rose", color: "pink", count: 10, price: 300, img: "./Images/flowers/Roses.png"},
+    {name: "Rose", color: "red", count: 10, price: 300, img: "./Images/flowers/Roses.png"},
+
+    {name: "Grass", color: "green", count: 10, price: 102.49, img: "./Images/flowers/Grass_1.png"},
+    {name: "Grass", color: "green", count: 10, price: 102.49, img: "./Images/flowers/Grass_2.png"},
+    {name: "Grass", color: "green", count: 10, price: 102.49, img: "./Images/flowers/Grass_3.png"},
+    {name: "Grass", color: "green", count: 10, price: 102.49, img: "./Images/flowers/Grass_4.png"},
+];
+
+let block = document.querySelector(".container");
+
+let flowersHtml = "";
+
+flowers.forEach(fl => {
+    flowersHtml += `
+    <div class="flower ${fl.name}"
+        style="background-color: ${fl.color}">
+        <h3>${fl.name}</h3>
+        <img src="${fl.img}" alt="${fl.name}" width="50px" height="50px">
+        <p>Цена: ${fl.price}</p>
+        <p>В наличии: ${fl.count}</p>
+    </div>       
+    `
+});
+
+// Вставляем в элемент данные из массива объектов, чтобы отобразить их
+block.innerHTML = flowersHtml;
+
