d43efd05d18053c17d507289e0768f2a3a5535d8
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-21 16:07:03 +0300

    Решена 3 КТ

--- website/static/js/snow.js
+++ website/static/js/snow.js
@@ -1,9 +1,22 @@
-/*
-
 // Код для метели на сайте
 
 let flakes_count = 25;
 let isFlurry = true;
+
+// Метель на сайте
+// jQuery(document).ready(function($){
+
+
+// });
+
+$(document).snowFlurry({
+    maxSize: 5,
+    numberOfFlakes: flakes_count,
+    minSpeed: 10,
+    maxSpeed: 15,
+    color: '#fff',
+    timeout: 0
+});
 
 // Включение / отключение метели
 function press()
@@ -13,25 +26,17 @@ function press()
         isFlurry = true;
         flakes_count = 25;
         console.log("isFlurry = " + isFlurry);
+        console.log("flakes_count = " + flakes_count);
+
     }
     else if (isFlurry === true)
     {
         isFlurry = false;
         flakes_count = 0;
         console.log("isFlurry = " + isFlurry);
+        console.log("flakes_count = " + flakes_count);
     }
-}
+};
 
-// Метель на сайте
-jQuery(document).ready(function($){
 
-    $(document).snowFlurry({
-        maxSize: 5,
-        numberOfFlakes: flakes_count,
-        minSpeed: 10,
-        maxSpeed: 15,
-        color: '#fff',
-        timeout: 0
-    });
-});
-*/
+
